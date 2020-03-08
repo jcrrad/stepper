49c93f8e9cb3d382d46f7cb63871e3ae0f76b1a0
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2020-03-08 17:16:43 -0400

    pulled out Stpper into its own class

---
+++ src/main/java/Stepper.java
@@ -0,0 +1,38 @@
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import jdk.nashorn.internal.runtime.ECMAException;
+
+public class Stepper {
+
+	private final GpioController gpio = GpioFactory.getInstance();
+
+	private final GpioPinDigitalOutput pulse = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "pulse+",
+			PinState.HIGH);
+
+	private final GpioPinDigitalOutput step = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "step+", PinState.HIGH);
+
+	public Stepper()
+	{
+		System.out.println("new Stepper made");
+	}
+
+	public void spin(int num)
+	{
+		try {
+			for (int i = 0; i < num; i++) {
+				System.out.println("HIGH");
+				step.high();
+				Thread.sleep(500);
+				System.out.println("HIGH");
+				step.low();
+				Thread.sleep(500);
+			}
+		}catch(Exception e)
+		{
+			System.out.println("Error: " + e);
+		}
+	}
+}
