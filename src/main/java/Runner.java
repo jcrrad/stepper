c63f13878b8531eae47164d4f54a2fe6928eb7fd
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2020-03-08 17:18:49 -0400

    Testing stepper on Pi

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,6 +3,6 @@ public class Runner {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Starting app");
-//		Stepper s = new Stepper();
+		Stepper s = new Stepper();
 	}
 }
