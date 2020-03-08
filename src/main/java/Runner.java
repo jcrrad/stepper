7065b00fc49916343c57a33fb364ad90cdb7991f
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2020-03-08 17:26:22 -0400

    spin motor

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,5 +4,6 @@ public class Runner {
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Starting app");
 		Stepper s = new Stepper();
+		s.spin(800);
 	}
 }
